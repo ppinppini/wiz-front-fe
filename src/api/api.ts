1db466265ff9b35c80baa1a60e6ae9de6c185a1a
heee1 <p1xell@kakao.com>
2024-09-10 02:13:06 +0900

    [Feat] MainPageGameSchedule.tsx에서 호출하는 api코드 분리작성 - MainPageGameSchedule.tsx에서 호출하는 api코드를 api.ts   파일에 분리하여 작성하였습니다. - 앞으로 작성되는 api호출코드를 이곳에 모아놓는 방향으로 개발할 예정입니다.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,17 +1,20 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export const api = {
-    getRecentGame: () => {
-        return fetch(`${API_BASE_URL}/game/recentGames`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Failed to fetch data');
-                }
-                return response.json();
-            })
-            .then((result) => result.data)
-            .catch((error) => {
-                throw new Error(error.message);
-            });
-    }   
-}
+    getRecentGame: async () => {
+        try {
+            const response = await fetch(`${API_BASE_URL}/game/recentGames`);
+            if (!response.ok) {
+                throw new Error('데이터를 불러오는데 실패했습니다.');
+            }
+            const responseData = await response.json();
+            return responseData.data;
+        } catch (error) {
+            if (error instanceof Error) {
+              throw new Error(error.message);
+            } else {
+              throw new Error('알 수 없는 에러 발생');
+            }
+        }
+    },
+};
