0b72d0b8a1599359c567e813ef37b95524ec7435
heee1 <p1xell@kakao.com>
2024-09-10 12:09:15 +0900

    [Feat] 하이라이트 비디오 api요청 코드 추가 - 최근경기정보 옆에 하이라이트 비디오의 고유 id값을 받는 코드를 추가하였습니다 - 관련해서 api호출코드를 api.ts 파일에 추가하였습니다. - 하이라이트 비디오 api 호출에 대한 response값의 타입을 types.ts에 추가하였습니다

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,20 +1,36 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const api = {
-    getRecentGame: async () => {
-        try {
-            const response = await fetch(`${API_BASE_URL}/game/recentGames`);
-            if (!response.ok) {
-                throw new Error('데이터를 불러오는데 실패했습니다.');
-            }
-            const responseData = await response.json();
-            return responseData.data;
-        } catch (error) {
-            if (error instanceof Error) {
-              throw new Error(error.message);
-            } else {
-              throw new Error('알 수 없는 에러 발생');
-            }
-        }
-    },
+  getRecentGame: async () => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/game/recentGames`);
+      if (!response.ok) {
+        throw new Error("데이터를 불러오는데 실패했습니다.");
+      }
+      const responseData = await response.json();
+      return responseData.data;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      } else {
+        throw new Error("알 수 없는 에러 발생");
+      }
+    }
+  },
+  getHighlightVideo: async () => {
+    try {
+      const response = await fetch(`${API_BASE_URL}/media/highlightVideo`);
+      if (!response.ok) {
+        throw new Error("데이터를 불러오는데 실패했습니다.");
+      }
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      } else {
+        throw new Error("알 수 없는 에러 발생");
+      }
+    }
+  },
 };
