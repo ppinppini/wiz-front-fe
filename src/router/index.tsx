184c27b5569b705b45e51531d43d88157b517af9
황유빈 <97329194+ppinppini@users.noreply.github.com>
2024-09-10 12:27:34 +0900

    [Feat] 메인 페이지 라우터 경로 설정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,12 +4,17 @@ import Nav from "../components/Navbar";
 import KtWizHistory from "../pages/info/History";
 import KtWizAbout from "../pages/info/About";
 import Schedule from "../pages/game/Schedule";
+import Main from "../pages/main/Main";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Nav />,
         children: [
+            {
+                path: "/",
+                element: <Main />,
+            },
             {
                 path: "/ktwiz/about",
                 element: <KtWizAbout />,
