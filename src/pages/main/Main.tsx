07233c83799be37f58eb7809ab9930a63cd954a6
chaehyeonkim <rlacoguskch603@naver.com>
2024-09-13 17:31:14 +0900

    [Feat] 메인에 게임 랭킹, 주차 안내 상단 카드 추가

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,19 +1,18 @@
-
 import MainBottomCard from "../../components/main/MainBottomCard";
 import MainHeader from "../../components/main/MainHeader";
 import MainHighlightsVideo from "../../components/main/MainHighlightsVideo";
 import MainPageGameSchedule from "../../components/main/MainPageGameSchedule";
-import Slide from '../../components/Slide';
+import Slide from "../../components/Slide";
 
 const Main = () => {
-    return (
-        <>
-            <MainHeader />
-            <MainPageGameSchedule />
-            <MainHighlightsVideo />
-            <Slide />
-            <MainBottomCard/>
-        </>
-    );
+  return (
+    <>
+      <MainHeader />
+      <MainPageGameSchedule />
+      <MainHighlightsVideo />
+      <Slide />
+      <MainBottomCard />
+    </>
+  );
 };
 export default Main;
