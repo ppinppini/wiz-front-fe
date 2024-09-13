548673cd4292652e5fe190e98adaf73b6685e708
신성우 <tlstjd1007@naver.com>
2024-09-13 16:52:18 +0900

    [Feat] index.tsx 원래대로

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -7,12 +7,7 @@ import KtWizAbout from "../pages/info/About";
 import Layouts from "../layouts/Layouts";
 //import Slide from '../components/Slide';
 
-<<<<<<< HEAD
-import KtWizHistory from "../pages/info/History";
-import KtWizAbout from "../pages/info/About";
-import Schedule from "../pages/game/Schedule";
-=======
->>>>>>> 023018f4913fc12f8bbd2954815da6481f02c7ba
+
 import Main from "../pages/main/Main";
 import Schedule from "../pages/game/Schedule";
 import RankingRecord from "../pages/game/RankingRecord";
