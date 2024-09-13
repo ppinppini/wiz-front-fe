47a6163f601fffcf319bd745ec318c61c3670bbd
황유빈 <97329194+ppinppini@users.noreply.github.com>
2024-09-13 16:35:05 +0900

    router import 경로 수정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Nav from "../components/Navbar";
 
-import KtWizHistory from "../pages/info/History";
-import KtWizAbout from "../pages/info/About";
+import KtWizHistory from "../pages/info/History";
+import KtWizAbout from "../pages/info/About";
 import Schedule from "../pages/game/Schedule";
 import Main from "../pages/main/Main";
 
