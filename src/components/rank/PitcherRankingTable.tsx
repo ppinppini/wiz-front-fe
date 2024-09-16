fa2904f5a3c9d1af66a40bcdb58e14909eb15640
heee1 <p1xell@kakao.com>
2024-09-16 20:32:09 +0900

    [Feat] 팀 투수순위 테이블, 팀 타자순위 테이블 컴포넌트 구현 - 팀 투수순위 테이블과 팀 타자순위 테이블을   컴포넌트로 구현하였습니다. - 테이블을 반환하는 컴포넌트이며,   상위 컴포넌트로 부터 관련 api데이터를 props로 받습니다.

---
+++ src/components/rank/PitcherRankingTable.tsx
@@ -0,0 +1,118 @@
+import {
+  getCoreRowModel,
+  useReactTable,
+  ColumnDef,
+} from "@tanstack/react-table";
+import { TGameSeasonTeamPitcherRank } from "../../types/types";
+
+const PitcherRankingTable: React.FC<{
+  pitcherRanking: TGameSeasonTeamPitcherRank;
+}> = ({ pitcherRanking }) => {
+  const columns: ColumnDef<TGameSeasonTeamPitcherRank[0]>[] = [
+    {
+      accessorKey: "teamName",
+      header: "팀명",
+    },
+    {
+      accessorKey: "sh",
+      header: "희타",
+    },
+    {
+      accessorKey: "sf",
+      header: "희비",
+    },
+    {
+      accessorKey: "bb",
+      header: "볼넷",
+    },
+    {
+      accessorKey: "ib",
+      header: "고의4구",
+    },
+    {
+      accessorKey: "hp",
+      header: "사구",
+    },
+    {
+      accessorKey: "kk",
+      header: "탈삼진",
+    },
+    {
+      accessorKey: "wp",
+      header: "폭투",
+    },
+    {
+      accessorKey: "bk",
+      header: "보크",
+    },
+    {
+      accessorKey: "r",
+      header: "실점",
+    },
+    {
+      accessorKey: "er",
+      header: "자책점",
+    },
+    {
+      accessorKey: "bs",
+      header: "블론세이브",
+    },
+    {
+      accessorKey: "whip",
+      header: "WHIP",
+    },
+    {
+      accessorKey: "oavg",
+      header: "피안타율",
+    },
+    {
+      accessorKey: "qs",
+      header: "QS",
+    },
+  ];
+
+  const table = useReactTable({
+    data: pitcherRanking,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className='text-xs text-center border-t-2 border-t-[#DC2626]'>
+      <table className='w-full border border-gray-200'>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id} className='bg-gray-100'>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id} className='border px-4 py-2'>
+                  {typeof header.column.columnDef.header === "function"
+                    ? header.column.columnDef.header(header.getContext())
+                    : header.column.columnDef.header}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr
+              key={row.id}
+              className={
+                row.getValue("teamName") === "KT"
+                  ? "bg-red-100 text-[#EC090C]"
+                  : "bg-white"
+              }
+            >
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id} className='border px-4 py-2'>
+                  {String(cell.getValue())}{" "}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default PitcherRankingTable;
