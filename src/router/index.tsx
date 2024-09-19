9b3cdd463290c51b4bbd3631d1882a8183999686
heee1 <p1xell@kakao.com>
2024-09-19 10:13:52 +0900

    [Chore] 병합 전 임시커밋2

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,42 +3,36 @@ import { createBrowserRouter } from "react-router-dom";
 
 import KtWizHistory from "../pages/info/History";
 import KtWizAbout from "../pages/info/About";
+import Schedule from "../pages/game/Schedule";
 
 import Layouts from "../layouts/Layouts";
 //import Slide from '../components/Slide';
 
 import Main from "../pages/main/Main";
-import Schedule from "../pages/game/Schedule";
-import RankingRecord from "../pages/game/RankingRecord";
 import Pitchersdetail from "../pages/players/details/Pitchersdetail";
 
-
 export const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Layouts />,
+    children: [
+      {
         path: "/",
-        element: <Layouts />,
-        children: [
-            {
-
-                path: "/",
-                element: <Main />,
-
-            },
-            {
-                path: "/ktwiz/about",
-                element: <KtWizAbout />,
-            },
-            {
-                path: "/ktwiz/history",
-                element: <KtWizHistory />,
-            },
-            {
-                path: "/game/schedule",
-                element: <Schedule />,
-            },
-            {   path: "/player/pitcher/details",
-                element: <Pitchersdetail/>,
-            }
-        ],
-    },
+        element: <Main />,
+      },
+      {
+        path: "/ktwiz/about",
+        element: <KtWizAbout />,
+      },
+      {
+        path: "/ktwiz/history",
+        element: <KtWizHistory />,
+      },
+      {
+        path: "/game/regular/schedule",
+        element: <Schedule />,
+      },
+      { path: "/player/pitcher/details", element: <Pitchersdetail /> },
+    ],
+  },
 ]);
