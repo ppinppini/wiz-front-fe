b6abbdb3adc785652041396f3c19edd8834ad405
신성우 <tlstjd1007@naver.com>
2024-09-24 15:12:42 +0900

    [Feat] 탭메뉴바, 텝메뉴네브바 선택시 표시

--- src/components/TabMenuNavbar.tsx
+++ src/components/TabMenuNavbar.tsx
@@ -5,20 +5,29 @@ type TabProps = {
     menuItems: {
         title: string;
         route: string;
-    }[];
+    }[],
+    tabtitle: string;
 };
 
-const TabMenuNavbar = ({ menuItems }: TabProps) => {
+const TabMenuNavbar = ({ menuItems, tabtitle }: TabProps) => {
     return (
-        <div className="fixed top-0 left-0 z-50 flex items-center justify-start w-full h-20 bg-black border-b bg-opacity-60 tab_menu border-white/70">
+        <div className="fixed top-0 left-0 z-50 flex items-center justify-start w-full h-20 bg-black border-b bg-opacity-60 border-white/70">
             <ul className="flex h-full items-center space-x-[34px] px-[140px]">
-                {menuItems.map((item, index) => (
-                    <li key={index} className="relative flex items-center h-full transition duration-200">
-                        <Link to={item.route} className="relative flex items-center h-full text-[17px] text-white transition duration-200 opacity-70 hover:opacity-100">
-                            {item.title}
-                        </Link>
-                    </li>
-                ))}
+                {menuItems.map((item, index) => {
+
+                    const isActive = tabtitle === item.title;
+
+                    return (
+                        <li key={index} className="relative flex items-center h-full transition duration-200">
+                            <Link 
+                              to={item.route} 
+                              className={`relative flex items-center h-full text-[17px] text-white transition duration-200 opacity-70 ${isActive ? 'tab_select' : 'tab_menu'}`}
+                            >
+                                {item.title}
+                            </Link>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     );
