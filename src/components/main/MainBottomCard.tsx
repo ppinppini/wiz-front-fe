0923a98817d339be705530a6f244652a77659569
heee1 <p1xell@kakao.com>
2024-09-25 16:23:07 +0900

    [Design] 코드 전체 검토 진행, 코드정리 & 스타일 수정 - 공통 	- 전체적인 코드를 검토하여 불필요한 주석 및 공백을 정리하였습니다. 	- 스타일이 어색한 부분을 수정하여 개선하였습니다.
    - 유빈님
    	- MainHighlightsVideo.tsx
    		- wiz video 백그라운드 이미지가 game shcedule가 동일한 느낌으로 출력되도록 수정
    		- 비디오리스트에 비디오 썸네일 img태그 상위에 div로 감싸서 overflow-clip을 부여하여
    		  쇼츠비디오 썸네일도 정상적으로 출력되도록 변경
    		- 버튼내부 텍스트에 text-sm font-extrabold를 부여하여 슬라이더쪽 버튼과 스타일 통일
    	- History.tsx
    		- 페이지 전체 heigth값을 넉넉하게 부여하여 최하단 흰색여백 제거
    
    - 성우님
    	- Slide.tsx
    		- 컨트롤러가 왼쪽으로 몰리는 현상 해결
    
    - 다은님
    	- PlayerCarousel.tsx
    		- api주소가 노출되어있는부분을 ${API_BASE_URL} 으로 수정
    
    - 채현님
    	- SearchBar.tsx
    		- 검색창에 mt-4를 부여하여 탭메뉴와의 붙어있는 부분 수정
    	- WizPress.tsx
    		- 메인컨테이너 외부에 탭태그가 위치하여 화면밖으로 벗어나는 문제 수정

--- src/components/main/MainBottomCard.tsx
+++ src/components/main/MainBottomCard.tsx
@@ -1,20 +1,18 @@
-/*컴포넌트 화면에서 확인하기위한
-임시 메인페이지*/
-
 import OnlineStoreCard from "../OnlineStoreCard";
 import PlayerOfTheMonthCard from "../PlayerOfTheMonthCard";
 
 const MainBottomCard = () => {
   return (
     <>
-    <div className="flex space-x-4 box-border
+      <div
+        className="flex space-x-4 box-border
     relative mx-auto w-full max-w-[1200px] px-[50px] mb-[160px]
-    mt-[140px]">
-    <OnlineStoreCard/>
-    <PlayerOfTheMonthCard/>
-    </div>
-    
+    mt-[140px]"
+      >
+        <OnlineStoreCard />
+        <PlayerOfTheMonthCard />
+      </div>
     </>
   );
-}
-export default MainBottomCard
+};
+export default MainBottomCard;
--- src/components/main/MainHighlightsVideo.tsx
+++ src/components/main/MainHighlightsVideo.tsx
@@ -22,23 +22,25 @@ const MainHighlightsVideo = () => {
   // 하이라이트 비디오 리스트를 보여주는 컴포넌트
   const MainHighlightsVideoList = () => {
     return (
-      <div className='flex flex-col items-center mt-40'>
-        <ul className='flex w-[68.75%] justify-between p-0 m-0'>
+      <div className="flex flex-col items-center mt-[50px]">
+        <ul className="flex w-[68.75em] h-[248px] justify-between p-0 m-0">
           {data.data.list
             .slice(1, 5)
             .map((video: TMainHighlightsVideoListType) => (
-              <li key={nanoid()} className='flex flex-col  w-[20%]'>
+              <li key={nanoid()} className="flex flex-col w-[23%]">
                 <Link to={"/"}>
-                  <img
-                    src={video.imgFilePath}
-                    alt='썸네일 이미지'
-                    className='block w-full h-[73px] rounded-md'
-                  />
-                  <div className='flex justify-between w-full  mt-2'>
-                    <div className='inline-block mb-4 rounded-[12px] bg-gradient-to-r from-[#f53232] via-[#cc65de] to-[#2ab2c6] px-2 py-1 text-[11px] text-white font-bold'>
+                  <div className="w-full h-[9.14em] overflow-clip">
+                    <img
+                      src={video.imgFilePath}
+                      alt="썸네일 이미지"
+                      className="block  rounded-md"
+                    />
+                  </div>
+                  <div className="flex justify-between w-full mt-2">
+                    <div className="inline-block mb-4 rounded-[0.75em] bg-gradient-to-r from-[#f53232] via-[#cc65de] to-[#2ab2c6] px-2 py-1 text-[0.6875em] text-white font-bold">
                       하이라이트
                     </div>
-                    <span className='block'>
+                    <span className="block text-sm text-gray-500">
                       {new Date(video.contentsDate).toLocaleDateString(
                         "ko-KR",
                         {
@@ -49,13 +51,13 @@ const MainHighlightsVideo = () => {
                       )}
                     </span>
                   </div>
-                  <h2 className='text-center'>{video.artcTitle}</h2>
+                  <h2 className="text-center">{video.artcTitle}</h2>
                 </Link>
               </li>
             ))}
         </ul>
 
-        <div className='px-4 py-2 mt-4 text-center border border-black rounded-md cursor-pointer w-80'>
+        <div className="px-4 py-2 mt-10 text-center border border-black rounded-md cursor-pointer w-80 text-sm font-extrabold">
           더 많은 영상 보기
         </div>
       </div>
@@ -63,19 +65,19 @@ const MainHighlightsVideo = () => {
   };
   return (
     <>
-      <div className='flex flex-col justify-between items-center min-h-[500px] relative z-10'>
+      <div className="flex flex-col justify-between items-center min-h-[31.25em] relative z-10">
         <img
-          className='absolute top-0 block'
-          src='https://www.ktwiz.co.kr/v2/imgs/img-title-video@2x.png'
-          alt='wizVideo이미지'
+          className="absolute top-0 block w-[36.25em] h-[8.75em]"
+          src="https://www.ktwiz.co.kr/v2/imgs/img-title-video@2x.png"
+          alt="wizVideo이미지"
         />
-        <div className='relative w-[68.75%] pb-[39%]'>
+        <div className="relative w-[68.75em] pb-[39%]">
           <iframe
             src={`https://tv.naver.com/embed/${data.data.list[0].refSeq}?autoPlay=false`}
             title={data.data.list[0].artTitle}
-            allow='autoplay'
+            allow="autoplay"
             allowFullScreen
-            className='absolute left-0 z-20 w-full h-full top-32 rounded-3xl'
+            className="absolute left-0 z-20 w-full h-[38.5em] top-32 rounded-3xl"
           ></iframe>
         </div>
       </div>
