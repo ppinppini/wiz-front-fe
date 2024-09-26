7d4ceea12281ce301024438ca77000ca4a91f2ad
heee1 <p1xell@kakao.com>
2024-09-27 00:25:28 +0900

    [Fix] 박스스코어 페이지 누락된 사항 수정 - 박스스코어 페이지에 누락되었던 탭 메뉴바, 탭 메뉴 네브바,   백그라운드 이미지, 페이지 로케이터를 추가하여   다른페이지와 구조 및 스타일을 통일하였습니다.

--- src/pages/game/BoxScore.tsx
+++ src/pages/game/BoxScore.tsx
@@ -5,14 +5,26 @@ import GameBoxScoreSchedule from "../../components/game/GameBoxScoreSchedule";
 import GameBoxScoreMainRecord from "../../components/game/GameBoxScoreMainRecord";
 import GameBoxScoreBatterRecord from "../../components/game/GameBoxScoreBatterRecord";
 import GameBoxScorePitcherRecord from "../../components/game/GameBoxScorePitcherRecord";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import BackgroundImage from "../../components/BackgroundImage";
+import playertogether from "../../assets/kt4.png";
+import TabMenuBar from "../../components/TabMenuBar";
+import TabMenuNavbar from "../../components/TabMenuNavbar";
+import PageLocator from "../../components/PageLocator";
 
 const BoxScore = () => {
     
     const [searchParams] = useSearchParams();
     const gameDate = searchParams.get("gameDate");
     const gmkey = searchParams.get("gmkey");
+    const [isSticky, setIsSticky] = useState(false);
+    const [hasAnimated, setHasAnimated] = useState(false);
 
+    const gameTabs = [
+        { title: "경기 일정", route: "../game/schedule" },
+        { title: "박스 스코어", route: "../game/boxScore" },
+        { title: "순위기록", route: "../game/ranking/team" },
+      ];
 
     if (!gameDate || !gmkey) {
         return <h1>데이터가 없습니다. URL이 잘못되었습니다.</h1>;
@@ -34,17 +46,56 @@ const BoxScore = () => {
     const homeName = scheduleMemo?.current?.home;
     const visitName = scheduleMemo?.current?.visit;
 
-    if (isError) return <h1>{error.message}</h1>;
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.scrollY >= 630) {
+            setIsSticky(true);
+          } else {
+            setIsSticky(false);
+            setHasAnimated(false);
+          }
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+    
+        // 클린업 함수에서 이벤트 리스너 제거
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
+    
     if (isLoading) return <h1> Loading ...</h1>;
 
     return (
         <div className="flex flex-col items-center gap-10 bg-black">
-            <GameBoxScoreSchedule scoreBoard={scoreBoardMemo} schedule={scheduleMemo} />
-            <GameBoxScoreMainRecord etcgames={mainRecordsMemo} />
-            <GameBoxScoreBatterRecord batters={hbattersMemo} name={homeName} />
-            <GameBoxScoreBatterRecord batters={vbattersMemo} name={visitName} />
-            <GameBoxScorePitcherRecord pitchers={hpitcherMemo} name={homeName} />
-            <GameBoxScorePitcherRecord pitchers={vpitcherMemo} name={visitName} />
+            <BackgroundImage
+          imageUrl={playertogether}
+          className="title-banner"
+          height="782px"
+        />
+        <TabMenuBar tabs={gameTabs} tabtitle="순위기록" />
+        {/* 스크롤 시 나타나는 TabMenuNavbar 컴포넌트 */}
+        {isSticky && (
+          <div
+            className={`fixed top-0 left-0 z-50 w-full ${!hasAnimated ? "animate-diagonal-slide" : ""}`}
+            onAnimationEnd={() => setHasAnimated(true)}
+          >
+            <TabMenuNavbar menuItems={gameTabs} tabtitle="순위기록" />
+          </div>
+        )}
+            <div>
+                {/* 페이지 로케이터 */}
+                <PageLocator
+                    pagePath="> Game > 정규 리그 > 순위 기록 > "
+                    currentPage="박스스코어"
+                />
+                <GameBoxScoreSchedule scoreBoard={scoreBoardMemo} schedule={scheduleMemo} />
+                <GameBoxScoreMainRecord etcgames={mainRecordsMemo} />
+                <GameBoxScoreBatterRecord batters={hbattersMemo} name={homeName} />
+                <GameBoxScoreBatterRecord batters={vbattersMemo} name={visitName} />
+                <GameBoxScorePitcherRecord pitchers={hpitcherMemo} name={homeName} />
+                <GameBoxScorePitcherRecord pitchers={vpitcherMemo} name={visitName} />
+            </div>
         </div>
     );
 };
